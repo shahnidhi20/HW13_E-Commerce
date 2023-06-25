20d070fe7f935fce36570ab9781d1e5376f3e0a8
Nidhi Shah <nidhi@nidhis-mbp.attlocal.net>
2023-06-25 13:02:53 -0700

    Added fields in the model and also associations

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,35 @@
-require('dotenv').config();
+const Sequelize = require("sequelize");
+const test = require("dotenv").config();
 
-const Sequelize = require('sequelize');
+let sequelize;
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  console.log("Config", test);
+
+  console.log(`${process.env.DB_NAME} ${process.env.DB_NAME}`);
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: "localhost",
+//       dialect: "mysql",
+//       port: 3306,
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
 
 module.exports = sequelize;
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,39 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category);
+
+//Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // Define an alias for when data is retrieved
+  as: "product_tags",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // Define an alias for when data is retrieved
+  as: "tag_products",
+});
 
 module.exports = {
   Product,
